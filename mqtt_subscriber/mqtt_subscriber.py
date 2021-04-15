c7cc7762f73a0ea21752af35f431f249339e19c2
Manuel Lanzersdorfer <20172016@atn.ac.at>
2021-04-15 10:52:32 +0200

    Update mqtt_subscriber.py
    write to file

--- mqtt_subscriber/mqtt_subscriber.py
+++ mqtt_subscriber/mqtt_subscriber.py
@@ -4,15 +4,18 @@ import paho.mqtt.client as mqtt
 
 topic_pos = "position_data"
 topic_sensor_data = "sensor_data"
+f = open("Test.txt", "w")
+
 
 def on_connect(client, userdata, flags, rc):
     print("Connected with result code " + str(rc))
-
     client.subscribe(topic_pos)
-    client.subscribe(topic_sensor_data)
+    #client.subscribe(topic_sensor_data)
 
 def on_message(client, userdata, msg):
-    print(msg.topic + " " + str(msg.payload))
+    f.write(str(msg.payload))
+    print(str(msg.payload))
+
 
 client = mqtt.Client()
 client.on_connect = on_connect
